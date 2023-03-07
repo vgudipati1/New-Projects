5474fd8defab49c74fc50962693e6a77c487edf4
vgudipati1 <106451659+vgudipati1@users.noreply.github.com>
2023-03-07 15:25:50 -0500

    Add files via upload

---
+++ password.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class password { 
+    //makeshift password database
+    static String[] passwordDatabase =
+    {"Sailaja1970", "Algernon2003!", 
+    "Algernon624!", "Sailaja1970!"};
+    
+    
+    public static void main(String []args){
+        Scanner username = new Scanner(System.in);
+        Scanner password = new Scanner(System.in);
+        System.out.println("Please enter Username:");
+        //phase 1 don't worry about checking username and if they point to a password
+        String userName = username.nextLine();
+        System.out.println("Username you entered is: " + userName);
+        
+        
+        System.out.println("Please enter Password:");
+        String passWord = password.nextLine();
+
+        if(passwordAccepted(passWord)== true){
+            System.out.println("Password is accepted  \n Access Granted!");
+        } else{
+            System.out.println("Incorrect password \n Access denied!");
+        }
+        
+        
+        
+        
+        //close resourse leaks
+        username.close(); 
+        password.close();
+
+    } 
+        
+    //Method to check if password is one of the acceptable passwords in our database
+    static boolean passwordAccepted(String passWord){
+        boolean isAccepted = false;
+        for (int i = 0; i < 4 ; i++){
+        if(passWord.equals(passwordDatabase[i])){
+            isAccepted = true;
+        }
+        
+        }
+        //return isAccepted;
+        return isAccepted; 
+    }
+    
+
+}
